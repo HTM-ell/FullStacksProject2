6cf7a99ff103959bd547fabb7481a578944f81ef
HTM-ell <elliott.e.foster@gmail.com>
2015-06-10 11:08:37 -0500

    Completed the final Task!!! Woot Woot!!

--- fullstack/vagrant/tournament/tournament.py
+++ fullstack/vagrant/tournament/tournament.py
@@ -35,7 +35,7 @@ def countPlayers():
 	"""Returns the number of players currently registered."""
 	conn = connect()
 	curs = conn.cursor()
-	curs.execute("Select Count(*) As Registered_Players From (Select * From players) as foo")
+	curs.execute("Select Count(*) As Registered_Players From players as foo")
 	RegPlayers = curs.fetchone()[0]
 	return RegPlayers
 	conn.close()
@@ -61,7 +61,7 @@ def playerStandings():
 
 	conn = connect()
 	curs = conn.cursor()
-	curs.execute("Select * From playerStandings ORDER BY matchesWon, player_name")
+	curs.execute("Select * From playerStandings ORDER BY matches_Won, player_name")
 	standings = curs.fetchall()
 	return standings
 	conn.close()
@@ -79,20 +79,25 @@ def reportMatch(winner, loser):
  
  
 def swissPairings():
-    """Returns a list of pairs of players for the next round of a match.
-  
-    Assuming that there are an even number of players registered, each player
-    appears exactly once in the pairings.  Each player is paired with another
-    player with an equal or nearly-equal win record, that is, a player adjacent
-    to him or her in the standings.
-  
-    Returns:
-      A list of tuples, each of which contains (id1, name1, id2, name2)
-        id1: the first player's unique id
-        name1: the first player's name
-        id2: the second player's unique id
-        name2: the second player's name
-    """
+    # Returns a list of pairs of players for the next round of a match.
+    # Assuming that there are an even number of players registered, each player
+    # appears exactly once in the pairings.  Each player is paired with another
+    # player with an equal or nearly-equal win record, that is, a player adjacent
+    # to him or her in the standings.
+    #  Returns:
+    # A list of tuples, each of which contains (id1, name1, id2, name2)
+    #    id1: the first player's unique id
+    #   name1: the first player's name
+    #    id2: the second player's unique id
+    #   name2: the second player's name1
+	
+	conn = connect()
+	curs = conn.cursor()
+	curs.execute("Select * from swissPairings")
+	pairings = curs.fetchall()
+	return pairings
+	conn.close()
+	
 
 
 
--- fullstack/vagrant/tournament/tournament.sql
+++ fullstack/vagrant/tournament/tournament.sql
@@ -19,6 +19,7 @@ match_ID SERIAL NOT NULL PRIMARY KEY,
 winner int ,
 loser int
 );
+
 CREATE VIEW playersStats
 AS
 SELECT p.player_ID, m.winner, m.loser, m.match_ID 
@@ -27,26 +28,26 @@ Right Outer Join players p  On p.player_ID=m.loser OR p.player_ID=m.winner;
 
 CREATE VIEW matchesPlayed
 AS
-SELECT player_ID, Count(match_ID) as matchesPlayed 
+SELECT player_ID, Count(match_ID) as matches_Played 
 From playersStats Group BY player_ID;
 
 CREATE VIEW matchesWon
 AS
-Select player_ID, Count(Distinct match_ID) As matchesWon From (Select a.player_ID, b.match_id
+Select player_ID, Count(Distinct match_ID) As matches_Won From (Select a.player_ID, b.match_id
 From playersStats a Left Outer Join playersStats b On a.player_ID = b.winner) as foo Group By player_ID;
 
 CREATE VIEW playerStandings
 AS
-Select p.player_ID, p.player_name, w.matchesWon, mp.matchesPlayed From players p 
+Select p.player_ID, p.player_name, w.matches_Won, mp.matches_Played From players p 
 join matchesWon w On p.player_ID = w.player_ID 
-join matchesPlayed mp ON w.player_ID = mp.player_ID;
+join matchesPlayed mp ON w.player_ID = mp.player_ID; 
 
+CREATE VIEW swissPairings
+AS
+select p1.player_id as P1_ID , p1.player_name AS P1_Name , p2.player_Id as P2_ID, p2.player_name as P2_Name From playerStandings p1
+Join playerStandings p2 on p1.matches_won = p2.matches_won and p1.player_ID < p2.player_ID
+WHERE p1.player_ID != p2.player_ID;
 
-
-
-
-
- 
 
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
