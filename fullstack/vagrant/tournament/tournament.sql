4b86fda14c5c8af519d264fabf4ef0db296bf636
HTM-ell <elliott.e.foster@gmail.com>
2015-06-24 16:43:50 -0500

    Corrected naming conventions in .sql file (no more camelCase)

--- fullstack/vagrant/tournament/tournament.sql
+++ fullstack/vagrant/tournament/tournament.sql
@@ -9,58 +9,54 @@ CREATE DATABASE tournament;
 \c tournament
 
 -- Commands to ensure that table are not already created.
-DROP TABLE players;
-DROP TABLE matches;
+DROP TABLE IF EXISTS players;
+DROP TABLE IF EXISTS matches;
 
 -- Command that creates the players table in which player's name and player ID or stored
 CREATE TABLE players(
-player_ID SERIAL NOT NULL PRIMARY KEY,
-player_Name varchar(25)
+player_id SERIAL NOT NULL PRIMARY KEY,
+player_name varchar(25)
 );
 
 -- Commad that creates the matches table in which the match ID, winning player ID and 
 -- losing player ID of each particular match are stored. 
 CREATE TABLE matches(
-match_ID SERIAL NOT NULL PRIMARY KEY,
-winner int references players(player_ID),
-loser int references players(player_ID)
+match_id SERIAL NOT NULL PRIMARY KEY,
+winner int references players(player_id),
+loser int references players(player_id)
 );
 
--- View that shows a player's ID number, match_ID and the winner and or loser of that match ID
-CREATE VIEW playersStats
+CREATE TABLE swiss_pairings
+
+-- View that shows a player's ID number, match_id and the winner and or loser of that match ID
+
+CREATE VIEW players_stats
 AS
-SELECT p.player_ID, m.winner, m.loser, m.match_ID 
+SELECT p.player_id, m.winner, m.loser, m.match_id 
 from matches m 
-Right Outer Join players p  On p.player_ID=m.loser OR p.player_ID=m.winner;
+Right Outer Join players p  On p.player_id=m.loser OR p.player_id=m.winner;
+
 
 -- View that shows the number of matches that were played by a particular player ID
-CREATE VIEW matchesPlayed
+CREATE VIEW matches_played
 AS
-SELECT player_ID, Count(match_ID) as matches_Played 
-From playersStats Group BY player_ID Order by player_ID;
+SELECT player_id, Count(match_id) as played_matches
+From players_stats Group BY player_id Order by player_id;
 
 --  View that shows the number of matches one per player ID
-CREATE VIEW matchesWon
+CREATE VIEW matches_won
 AS
-Select player_ID, Count(Distinct match_ID) As matches_Won From (Select a.player_ID, b.match_id
-From playersStats a Left Outer Join playersStats b On a.player_ID = b.winner) as foo Group By player_ID;
+Select player_id, Count(Distinct match_id) As won_matches From (Select a.player_id, b.match_id
+From players_stats a Left Outer Join players_stats b On a.player_id = b.winner) as combplayer_match Group By player_id;
 
 -- View that shows the name of a player, the number of matches they have won and played collectively per Player ID
-CREATE VIEW playerStandings
+CREATE VIEW player_standings
 AS
-Select p.player_ID, p.player_name, w.matches_Won, mp.matches_Played From players p 
-join matchesWon w On p.player_ID = w.player_ID 
-join matchesPlayed mp ON w.player_ID = mp.player_ID; 
+Select p.player_id, p.player_name, w.won_matches, mp.played_matches From players p 
+join matches_won w On p.player_id = w.player_id 
+join matches_played mp ON w.player_id = mp.player_id; 
 
 -- View that shows the pairings for a particular round of matches. 
-CREATE VIEW swissPairings
+CREATE VIEW swiss_pairings
 AS
-select p1.player_id as P1_ID , p1.player_name AS P1_Name , p2.player_Id as P2_ID, p2.player_name as P2_Name From playerStandings p1
-Join playerStandings p2 on p1.matches_won = p2.matches_won and p1.player_ID < p2.player_ID
-WHERE p1.player_ID != p2.player_ID;
-
-
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
-
-
+select p1.player_id as P1_ID , p1.player_name AS P1_Name , p2.player_id as P2_ID, p2.player_name as P2_Name From player_standings p1
